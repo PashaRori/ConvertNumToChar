411cc63ae7555c00474b0186836dcd5ffc731ab6
Pavel Knush <pavel_knush17@mail.ru>
2019-10-07 17:23:44 +0300

    Commit 20.09.2019

--- src/test/java/NumberToWordsTest.java
+++ src/test/java/NumberToWordsTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.*;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,13 +18,22 @@ class NumberToWordsTest {
         numberToWords = null;
     }
 
+    @DisplayName("Проверка для числа 100")
     @Test
     public void testConvertEquals() throws IOException {
         assertEquals("сто", numberToWords.convertNumberToWords(100));
     }
 
+    @DisplayName("Проверка с добущенной ошибкой")
     @Test
     public void testConvertNotEquals() throws IOException {
         assertEquals("семнадцать", numberToWords.convertNumberToWords(13));
+    }
+
+    @DisplayName("Проверка для числа -1 с таймером")
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    public void  testConvertTimer() throws IOException {
+        assertEquals("минус один", numberToWords.convertNumberToWords(-1));
     }
 }
