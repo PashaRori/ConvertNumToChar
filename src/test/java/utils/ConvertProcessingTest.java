1d2883a257a4554fbd5399daf25cd300511a5a04
Pavel Knush <pavel_knush17@mail.ru>
2019-10-15 22:22:12 +0300

    Commit 15.10.2019 22:19

--- src/test/java/utils/ConvertProcessingTest.java
+++ src/test/java/utils/ConvertProcessingTest.java
@@ -1,37 +1,83 @@
 package utils;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ConvertProcessingTest {/*
-    private static int numberSegment;
+class ConvertProcessingTest {
+    private static String numberSegment;
     private static int quantityOfNumberClasses;
-    private static int controlZerosEnding;
     private static String language;
+    private static String expectedNumber;
+    private static StringBuilder actualNumber;
 
-    @Test
-    void createOnRussian() {
-        ConvertProcessing.createOnRussian(numberSegment,quantityOfNumberClasses,controlZerosEnding,language);
+    @BeforeEach
+    public void createMemoryForTest() {
+        actualNumber = new StringBuilder();
+    }
+
+    @AfterEach
+    public void clearMemoryForTest() {
+        expectedNumber = null;
+        actualNumber = null;
     }
 
     @Test
-    void createOnRussian() {
+    void compareValueOneHundredTwentyThreeWihThatRussianWord() {
+        numberSegment = "123";
+        quantityOfNumberClasses = 0;
+        language = "Rus";
+        expectedNumber = " сто двадцать три";
+        actualNumber = ConvertProcessing.createOnRussian(numberSegment, quantityOfNumberClasses, language);
+        String actualNumber1 = actualNumber.toString();
+        assertEquals(expectedNumber, actualNumber1);
     }
 
     @Test
-    void createOnRussian() {
+    void compareValueOneHundredTwentyThreeThousandWihThatRussianWord() {
+        numberSegment = "123";
+        quantityOfNumberClasses = 1;
+        language = "Rus";
+        expectedNumber = " сто двадцать три тысячи";
+        actualNumber = ConvertProcessing.createOnRussian(numberSegment, quantityOfNumberClasses, language);
+        String actualNumber1 = actualNumber.toString();
+        assertEquals(expectedNumber, actualNumber1);
     }
 
+    /*
+        @Test
+        void createOnRussian() {
+            language = "Rus";
+            ConvertProcessing.createOnRussian(numberSegment,quantityOfNumberClasses,language);
+        }
+    */
+    @Test
+    void compareValueOneHundredTwentyThreeWihThatEnglishWord() {
+        numberSegment = "123";
+        quantityOfNumberClasses = 0;
+        language = "Eng";
+        expectedNumber = " one hundred and twenty three";
+        actualNumber = ConvertProcessing.createOnEnglish(numberSegment, quantityOfNumberClasses, language);
+        String actualNumber1 = actualNumber.toString();
+        assertEquals(expectedNumber, actualNumber1);
+    }
+
+    @Test
+    void compareValueOneHundredTwentyThreeThousandWihThatEnglishWord() {
+        numberSegment = "123";
+        quantityOfNumberClasses = 1;
+        language = "Eng";
+        expectedNumber = " one hundred twenty three thousand";
+        actualNumber = ConvertProcessing.createOnEnglish(numberSegment, quantityOfNumberClasses, language);
+        String actualNumber1 = actualNumber.toString();
+        assertEquals(expectedNumber, actualNumber1);
+    }
+/*
     @Test
     void createOnEnglish() {
-    }
-
-    @Test
-    void createOnEnglish() {
-    }
-
-    @Test
-    void createOnEnglish() {
+        language = "Eng";
+        ConvertProcessing.createOnRussian(numberSegment,quantityOfNumberClasses,language);
     }*/
 }
